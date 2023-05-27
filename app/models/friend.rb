200a6ce04f07f27bc66269fa7f91f103828935a9
DamianWieszczak <damian.wieszczak@gmail.com>
2023-05-27 18:51:12 +0200

    Add basic validations

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,5 @@
 class Friend < ApplicationRecord
+  validates :name, presence: true, length: { in: 3..50 }
+  validates :surname, presence: true, length: { in: 3..60 }
+  validates :email, presence: true, length: { in: 3..70 }
 end
